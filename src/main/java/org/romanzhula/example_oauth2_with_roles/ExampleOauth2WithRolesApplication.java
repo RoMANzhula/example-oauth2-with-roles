ee63e643c4b10c65dd7fe68b4715bea73c96b3b5
RoMANzhula <roman1985al@ukr.net>
2025-06-01 19:31:20 +0300

    Add ExampleOauth2WithRolesApplication

---
+++ src/main/java/org/romanzhula/example_oauth2_with_roles/ExampleOauth2WithRolesApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.example_oauth2_with_roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOauth2WithRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleOauth2WithRolesApplication.class, args);
+	}
+
+}
