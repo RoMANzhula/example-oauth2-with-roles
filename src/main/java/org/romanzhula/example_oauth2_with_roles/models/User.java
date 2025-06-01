70bd6a858c29d733fa2e07e18ae1ee46d47f274e
RoMANzhula <roman1985al@ukr.net>
2025-06-01 20:45:25 +0300

    Add User model

---
+++ src/main/java/org/romanzhula/example_oauth2_with_roles/models/User.java
@@ -0,0 +1,35 @@
+package org.romanzhula.example_oauth2_with_roles.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id")
+    )
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Role> roles;
+
+}
