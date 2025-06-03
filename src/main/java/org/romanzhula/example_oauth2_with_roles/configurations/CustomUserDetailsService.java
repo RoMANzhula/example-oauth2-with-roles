222a46fe951e4ba5f4c86d03c4817ddb96ee12c7
RoMANzhula <roman1985al@ukr.net>
2025-06-03 22:18:02 +0300

    Add CustomUserDetailsService - load user by email

---
+++ src/main/java/org/romanzhula/example_oauth2_with_roles/configurations/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package org.romanzhula.example_oauth2_with_roles.configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.example_oauth2_with_roles.repositories.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> new org.springframework.security.core.userdetails.User(
+                        user.getEmail(),
+                        user.getPassword(),
+                        user.getRoles().stream()
+                                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                                .collect(Collectors.toList())
+                        )
+                )
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"))
+        ;
+    }
+
+}
